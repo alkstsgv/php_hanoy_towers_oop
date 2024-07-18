b5107c8b18945b337bcdeb541107dcec75b1b7c0
alkstsgv <alkstsgv@yandex.ru>
2024-07-18 12:08:16 +1000

    init

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '35f0c4761cbae73ef4b924cb87448a4dc0edce58',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '35f0c4761cbae73ef4b924cb87448a4dc0edce58',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
